934dafe0fe4c5802c5ac1b718cea3ac3e2b25698
Vishal Hanchnoli <hanchnoli@gmail.com>
2023-03-15 21:27:46 +0530

    Update dbconn.php

--- old/dbconn.php
+++ old/dbconn.php
@@ -1,8 +1,15 @@
-<?php
-function dbconn(){
-    $conn=mysqli_connect("sql300.epizy.com","epiz_33274841","thogtOMEwhKSu6W","epiz_33274841_hms");
-    if(!$conn)
-        die("FAILED TO CONNECT TO DATABASE".mysqli_connect_error());
-    return $conn;
-}
+<?php
+
+function dbconn(){
+
+    $conn=mysqli_connect("","","","");
+
+    if(!$conn)
+
+        die("FAILED TO CONNECT TO DATABASE".mysqli_connect_error());
+
+    return $conn;
+
+}
+
 ?>
