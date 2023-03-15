8a183cd0d51950fe0715fa4b7adec2634e932c1f
Vishal Hanchnoli <hanchnoli@gmail.com>
2023-03-15 21:28:18 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function dbconn(){
-    $conn=mysqli_connect("sql300.epizy.com","epiz_33274841","thogtOMEwhKSu6W","epiz_33274841_hms");
+    $conn=mysqli_connect("","","","");
     if(!$conn)
         die("FAILED TO CONNECT TO DATABASE".mysqli_connect_error());
     return $conn;
